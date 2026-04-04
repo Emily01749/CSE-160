16e5fefb958a458b61e30a37bb68d64875ad3de1
emily <etmayer@gmail.com>
2026-04-03 23:34:20 -0700

    commenting console logs

--- asgn0/asg0.js
+++ asgn0/asg0.js
@@ -12,7 +12,7 @@ function main() {
 
   // Draw a blue rectangle
   ctx.fillStyle = 'rgba(0, 0, 0, 1.0)'; // Set color to black
-  ctx.fillRect(0, 0, canvas.width, canvas.height);        // Fill a rectangle with the color
+  ctx.fillRect(0, 0, canvas.width, canvas.height); // Fill a rectangle with the color
   
   var v1 = new Vector3();
   v1.elements[0] = 2.25;
@@ -123,13 +123,13 @@ function handleDrawOperationEvent(){
 
   if(optSelect.value == 'Add'){
     v3 = v1.add(v2);
-    console.log(v3);
+    //console.log(v3);
 
     drawVector(v3, "green");
   }
   if(optSelect.value == 'Subtract'){
     v3 = v1.sub(v2);
-    console.log(v3);
+    //console.log(v3);
 
     drawVector(v3, "green");
   }
@@ -139,12 +139,12 @@ function handleDrawOperationEvent(){
 
   if(optSelect.value == 'Multiply'){
     v3 = v1.mul(scal);
-    console.log(v3);
+    //console.log(v3);
 
     drawVector(v3, "green");
 
     v4 = v2.mul(scal);
-    console.log(v4);
+    //console.log(v4);
 
     drawVector(v4, "green");
 
@@ -152,12 +152,12 @@ function handleDrawOperationEvent(){
 
   if(optSelect.value == 'Divide'){
     v3 = v1.div(scal);
-    console.log(v3);
+    //console.log(v3);
 
     drawVector(v3, "green");
 
     v4 = v2.div(scal);
-    console.log(v4);
+    //console.log(v4);
 
     drawVector(v4, "green");
 
@@ -185,11 +185,11 @@ function handleDrawOperationEvent(){
 
 function angleBetween(v1,v2){
   var dotprd = Vector3.dot(v1,v2);
-  console.log("Dotdotprd: " + dotprd);
+  //console.log("Dotdotprd: " + dotprd);
   var mag = v1.magnitude() * v2.magnitude();
-  console.log("Mag: " + mag);
+  //console.log("Mag: " + mag);
   var theta = Math.acos(dotprd / mag);
-  console.log("Theta: " + theta);
+  //console.log("Theta: " + theta);
 
   var thetaInDegrees = theta * (180/Math.PI);
 
@@ -198,6 +198,6 @@ function angleBetween(v1,v2){
 
 function areaTriangle(v1, v2){
   let crossProd = Vector3.cross(v1, v2);
-  console.log("Cross: ", crossProd);
+  //console.log("Cross: ", crossProd);
   return crossProd.magnitude() / 2;
 }
