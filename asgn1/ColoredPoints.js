35875364aa786ff57736b01566dcde44e4b34b26
emily <etmayer@ucsc.edu>
2026-04-12 13:40:27 -0700

    added modified Triangle class; making my drawing & added button for it

--- asgn1/ColoredPoints.js
+++ asgn1/ColoredPoints.js
@@ -123,6 +123,70 @@ function addUI(){
     g_circleSegmentNum = this.value;
   })
 
+  
+  document.getElementById('art').addEventListener('mouseup', function() {
+    console.log("art button");
+    /*
+    <path id="shape0" 
+    transform="translate(457.387580299786, 310.920770877944)" 
+    fill="none" stroke="#000000" stroke-width="3" stroke-linecap="square" stroke-linejoin="bevel" 
+    d="M199.968 236.403L0 23.1263L339.186 0Z" 
+    sodipodi:nodetypes="cccc"/><path id="shape1" 
+    transform="translate(452.248394004283, 195.289079229122)" 
+    fill="none" stroke="#000000" stroke-width="3" stroke-linecap="square" stroke-linejoin="bevel" 
+    d="M0 141.328L56.531 0L151.606 131.049Z" sodipodi:nodetypes="cccc"/><path id="shape2" 
+    transform="translate(696.359743040685, 172.162740899358)" 
+    fill="none" stroke="#000000" stroke-width="3" stroke-linecap="square" stroke-linejoin="bevel" 
+    d="M0 143.897L56.531 0L105.353 141.328Z" sodipodi:nodetypes="cccc"/>
+    */
+    let t;
+    let scale = 1500;
+    let drawingPts = [
+      [[1292, 1116], [907, 775], [1677, 759], [1.0, 1.0, 1.0, 1.0]], // head
+      [[907, 780], [1105, 401], [1237, 773], [1.0, 1.0, 1.0, 1.0]], // left ear
+      [[1424, 759], [1677, 759], [1589, 390], [1.0, 1.0, 1.0, 1.0]], // right ear
+      [[1133, 995], [1375, 1320], [1287, 1116], [1.0, 1.0, 1.0, 1.0]], // left neck
+      [[1298, 1325], [1567, 1105], [1430, 968], [1.0, 1.0, 1.0, 1.0]], // right neck
+      [[1567, 1105], [2018, 1655], [1276, 1342], [1.0, 1.0, 1.0, 1.0]], // body
+      [[1270, 1331], [1446, 1545], [1276, 1771], [1.0, 1.0, 1.0, 1.0]], // front leg (back)
+      [[1578, 1463], [1688, 1540], [1496, 1776], [1.0, 1.0, 1.0, 1.0]], // front leg (front)
+      [[2018, 1650], [1727, 2205], [2233, 1622], [1.0, 1.0, 1.0, 1.0]], // Tail 1
+      [[1259, 1754], [891, 1457], [896, 1881], [1.0, 1.0, 1.0, 1.0]], // Tail 2 (first part)
+      [[891, 1457], [302, 1996], [885, 1908], [1.0, 1.0, 1.0, 1.0]], // Tail 2 (2nd part)
+      [[1771, 1408], [1859, 1325], [1875, 1435], [1.0, 1.0, 1.0, 1.0]], // "E" (1)
+      [[1875, 1325], [1974, 1380], [1870, 1424], [1.0, 1.0, 1.0, 1.0]], // "E" (2)
+      [[1864, 1325], [1969, 1380], [1864, 1325], [1.0, 1.0, 1.0, 1.0]], // "E" (3)
+      [[1870, 1331], [1974, 1237], [1853, 1215], [1.0, 1.0, 1.0, 1.0]], // "E" (4)
+      [[1859, 1204], [1853, 1111], [1969, 1171], [1.0, 1.0, 1.0, 1.0]], // "E" (5)
+      [[1980, 1402], [2084, 1325], [2101, 1408], [1.0, 1.0, 1.0, 1.0]], // "M" (1)
+      [[2101, 1320], [2183, 1182], [2189, 1325], [1.0, 1.0, 1.0, 1.0]], // "M" (2)
+      [[2189, 1204], [2183, 1325], [2304, 1320], [1.0, 1.0, 1.0, 1.0]], // "M" (3)
+      [[2233, 1325], [2304, 1314], [2326, 1419], [1.0, 1.0, 1.0, 1.0]], // "M" (4)
+      [[2348, 1309], [2409, 1210], [2442, 1314], [1.0, 1.0, 1.0, 1.0]], // "M" (5)
+      [[2343, 1336], [2442, 1446], [2442, 1320], [1.0, 1.0, 1.0, 1.0]], // "M" (6)
+    ]
+
+    let offsetX = -1;
+
+    for(let i = 0; i < drawingPts.length; i++){
+      let smth = drawingPts[i];
+      let p1 = [smth[0][0]/scale + offsetX, 1 - smth[0][1]/scale];
+      let p2 = [smth[1][0]/scale + offsetX, 1 - smth[1][1]/scale];
+      let p3 = [smth[2][0]/scale + offsetX, 1 - smth[2][1]/scale];
+      t = new ModifiedTriangle(p1, p2, p3, smth[3]);
+      g_shapesList.push(t);
+    }
+    //t = new ModifiedTriangle([0,0], [0.25, 0.25], [-0.25, 0.25], [1.0, 1.0, 1.0, 1.0]); g_shapesList.push(t);
+    
+    //t = new ModifiedTriangle([-0.25, 0.25], [0.85, 0.25], [1, 0.25], [1.0, 1.0, 1.0, 1.0]); g_shapesList.push(t);
+
+    console.log(g_shapesList);
+
+    renderAllShapes();
+
+
+  })
+
   g_selectedColor[0] = document.getElementById('redSlide').value/100;
   g_selectedColor[1] = document.getElementById('greenSlide').value/100;
   g_selectedColor[2] = document.getElementById('blueSlide').value/100;
@@ -158,7 +222,6 @@ function main() {
 }
 
 var g_shapesList = [];
-
 /*var g_points = [];  // The array for the position of a mouse press
 var g_colors = [];  // The array to store the color of a point
 var g_sizes = [];*/
